d593bf557d47858145f9e5ed71efb8da805d9376
Artem Vlasenko <artem.vlasenko@gmail.com>
2017-09-01 12:30:34 +0300

    initial commit

---
+++ src/main/java/rialto/rialto/RialtoApplication.java
@@ -0,0 +1,12 @@
+package rialto.rialto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RialtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RialtoApplication.class, args);
+	}
+}
